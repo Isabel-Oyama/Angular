595787887eb3a01370fb5bb35ba341137a2b05c6
Isabel-Oyama <emiko.oyama93@gmail.com>
2021-03-01 19:59:18 -0300

    Implementação do post e get de postagens

--- blogPessoal/src/app/service/auth.service.ts
+++ blogPessoal/src/app/service/auth.service.ts
@@ -1,10 +1,9 @@
-
+import { environment } from './../../environments/environment.prod';
+import { UserLogin } from './../model/UserLogin';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
-import { UserLogin } from '../model/UserLogin';
 
 @Injectable({
   providedIn: 'root'
@@ -23,14 +22,19 @@ export class AuthService {
     return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
   }
 
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`http://localhost:8080/usuarios/${id}`)
+  }
+
+
   logado(){
     let ok: boolean = false
-    
-    if(environment.token != ''){
+
+    if (environment.token != ''){
       ok = true
-    
     }
 
     return ok
   }
+
 }
